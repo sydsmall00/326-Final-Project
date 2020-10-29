c02a4686b2febcccc13e8c0834056b55ea2d4774
Sydney Small <ssmall1@terpmail.umd.edu>
2020-10-29 13:42:42 -0400

    Outlined the script and added proper Classes.

---
+++ crazyeights.py
@@ -0,0 +1,43 @@
+# Programmers: Sydney Small, Dag Asfaw, Fernando Sierra, Aaron Rapaka
+
+from random import shuffle
+
+class Card:
+    """ Initialize the deck of cards.
+        
+        Attributes:
+        
+        """
+class Deck:
+    """ Deck of cards in order.
+    
+        Attributes:
+        
+        """
+class Action:
+    """ Creates functions for actions to be taken in the game.
+    
+        Attributes:
+        
+        """
+class Player:
+    """ Uses Action class for player's turn.
+    
+        Attributes:
+        
+        """
+class Computer:
+    """ Uses Action class for the computer opponent's turn.
+    
+        Attributes:
+        
+        """
+class Game:
+    """ Crazy Eights game.
+    
+        Attributes: 
+        
+        """
+
+if __name__ == "__main__":
+    Game().play()
