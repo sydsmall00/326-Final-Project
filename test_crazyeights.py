c4e91b1b5c34125c0d8870490519b6c2156195cc
sydsmall00 <73247954+sydsmall00@users.noreply.github.com>
2020-12-15 16:37:23 -0500

    Wrote test_init() method.

--- test_crazyeights.py
+++ test_crazyeights.py
@@ -1,16 +1,21 @@
 # Programmers: Dagmawit Asfaw, Aaron Rapaka, Fernando Sierra, Sydney Small
 
-import pytest
 from crazyeights import Game
+import pytest
 
-def test_player_options():
-    # test whether the player hand is a list
-    assert isinstance(self.p_hand, list), "Player hand should be a list."
-    # test whether the discard pile is displaying a card
-    assert len(self.discarded) > 0, "Discard pile has cards to play with."
-    # test selected card is either new card or number of card to be discarded
-    assert isinstance(selected_card, int or str)
-    # test if a player plays an 8 that the suit has changed
-    assert 
-    # test if the suit is changed that it is not changed after player turn
-    assert
+def test_init():
+    g = Game()
+    assert isinstance(g.suits, list), "The suits attribute should be a list of suits."
+    assert len(g.suits) == 4, "There should be 4 suits."
+    assert isinstance(g.suits[0:], str), "Each suit should be a string."
+    assert isinstance(g.faces, list), "The faces attribute should be a list of faces."
+    assert len(g.faces) == 13, "There should be 13 faces."
+    assert isinstance(g.faces[0:], str), "Each face should be a string."
+    assert isinstance(g.value, int), "The value attribute should be an integer."
+    assert isinstance(g.deck, list), "The deck should be a list."
+    assert isinstance(g.deck[0:], tuple), "The deck should hold cards as tuples."
+    for attr in ["suit", "face"]:
+        assert hasattr(g.deck[0], attr), "The cards should be a suit, face"
+    assert len(g.p_hand) == 0, "Player hand should have no objects."
+    assert len(g.computer_hand) == 0, "Computer hand should have no objects."
+    assert len(g.discarded) == 0, "The discard pile should have no objects."
